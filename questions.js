21868bf667f9d62726d184743c8913c4244650b0
Danzinbbx <Danilodamascenopereira2008@gmail.com>
2024-09-25 19:25:47 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,34 +1,123 @@
 export default [
   {
-    question: "Quanto é 1 + 2?",
+    question: " Quais foram os principais aliados durante a Segunda Guerra Mundial?
+",
     answers: [
-      { option: "3", correct: true },
-      { option: "2", correct: false },
-      { option: "4", correct: false },
+      { option: "França, Inglaterra, EUA e União Soviética
+", correct: true },
+      { option: "Alemanha, França, Japão e Espanha
+", correct: false },
+      { option: "França, Japão, União Soviética e Itália", correct: false },
     ],
   },
   {
-    question: "Quanto é 5 x 2?",
+    question: "Quais foram os países que compuseram as Potências do Eixo na Segunda Guerra Mundial?",
     answers: [
-      { option: "9", correct: false },
-      { option: "10", correct: true },
-      { option: "11", correct: false },
+      { option: "Itália, Alemanha e Inglaterra 
+", correct: false },
+      { option: "Alemanha, Japão e Polônia
+", correct: false },
+      { option: "Alemanha, Itália e Japão", correct: true },
     ],
   },
   {
-    question: "Quanto é 10 x 8?",
+    question: "Quando iniciou a Segunda Guerra Mundial?
+",
     answers: [
-      { option: "800", correct: false },
-      { option: "90", correct: false },
-      { option: "80", correct: true },
+      { option: "2 de Agosto de 1939
+", correct: false },
+      { option: "1 de Setembro de 1939", correct: true },
+      { option: "2 de Outubro de 1939", correct: false },
     ],
   },
   {
-    question: "Quanto é 7 x 7?",
+    question: "Qual foi o estopim desse Conflito?
+",
     answers: [
-      { option: "70", correct: false },
-      { option: "56", correct: false },
-      { option: "49", correct: true },
+      { option: "Conflitos com Aliados
+", correct: false },
+      { option: "A invasão da Polônia pelos Alemães", correct: true },
+      { option: "Disputa Territoria", correct: false },
     ],
   },
 ];
+{
+    question: "Linha de Maginot era o sistema de defesa de qual país europeu?
+?
+",
+    answers: [
+      { option: "França", correct: true },
+      { option: "União Soviética", correct: false },
+      { option: "Alemanha", correct: false },
+    ],
+  },
+];
+
+{
+    question: "Quais cidades mais memoráveis a serem atacadas na época da segunda guerra mundial??
+",
+    answers: [
+      { option: "Hiroshima e Nagasaki.", correct: true },
+      { option: "Varsóvia e Dresden.
+", correct: false },
+      { option: "Roterdã e Milão.
+", correct: false },
+    ],
+  },
+];
+{
+    question: "Quase foram os principais teatros na Segunda Guerra Mundial?
+?
+",
+    answers: [
+      { option: "Europa, Pacífico, Norte da África, Ásia e América.", correct: false },
+      { option: "Europa, Pacífico, Sul da África e Ásia.", correct: false },
+      { option: "Europa, Pacífico, Norte da África e Ásia.
+", correct: true },
+    ],
+  },
+];
+
+{
+    question: "Como foi denominada a Perseguição sistemática e o assassinato de 6 milhões de judeus?
+?
+",
+    answers: [
+      { option: "Holocausto.
+", correct: true },
+      { option: "Guerra Fria.
+", correct: false },
+      { option: "Regime Totalitário.", correct: false },
+    ],
+  },
+];
+
+{
+    question: "Quais as consequências geradas pela Segunda Guerra Mundial?
+?
+",
+    answers: [
+      { option: "Revolução Industrial, Fim da Guerra Fria, Divisão territorial e Criação da ONU.
+", correct: false },
+      { option: "Holocausto e Fim da Guerra Fria.", correct: false },
+      { option: "Queda do Regime Totalitário, Criação da ONU, início da Guerra Fria e redefinição das fronteiras globais.
+", correct: true },
+    ],
+  },
+];
+
+{
+    question: "Quais as duas alianças divididas durante a Segunda Guerra Mundial?
+?
+",
+    answers: [
+      { option: "Aliados e eixo", correct: false },
+      { option: "Tríplice Aliança e Tríplice Entente 
+", correct: true },
+      { option: "Eixo e Aliança", correct: false },
+    ],
+  },
+];
+
+
+
